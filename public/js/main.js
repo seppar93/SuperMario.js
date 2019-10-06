7b5a1a2ecc007f3aa5ccfca86d7c231aac4a30bb
Sepehr Parirokh <sepehrparirokh@gmail.com>
2019-10-06 14:18:07 -0700

    added modules:

---
+++ public/js/main.js
@@ -0,0 +1,55 @@
+function loadImage(url) {
+  return new Promise(resolve => {
+    const image = new Image()
+    image.addEventListener('load', () => {
+      resolve(image)
+    })
+    image.src = url
+  })
+}
+
+class SpriteSheet {
+  constructor(image, width, height) {
+    this.image = image
+    this.width = width
+    this.height = height
+    this.tiles = new Map()
+  }
+  define(name, x, y) { // canvas made programmatically
+    const buffer = document.createElement('canvas')
+    buffer.width = this.width
+    buffer.height = this.height
+    buffer.getContext('2d').
+      drawImage(this.image,
+        x * this.width,
+        y * this.height,
+        this.width, this.height,
+        0, 0, this.width, this.height);
+
+    this.tiles.set(name, buffer);
+  }
+  draw(name, context, x, y) {
+    const buffer = this.tiles.get(name)
+    context.drawImage(buffer, x, y)
+  }
+}
+
+const canvas = document.getElementById('screen')
+const context = canvas.getContext('2d')
+
+context.fillRect(0, 0, 50, 50)
+
+loadImage('/img/tiles.png').then(image => {
+  const sprites = new SpriteSheet(image, 16, 16) // tile size 16
+  sprites.define('ground', 0, 0)
+  sprites.draw('ground', context, 45, 62)
+
+
+
+
+  // context.drawImage(image,
+  //   0, 0, 16, 16, // subset you want
+  //   0, 0, // where to draw
+  //   16, 16 // size
+  // )
+})
