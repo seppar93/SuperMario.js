4780b185f83c2cb5d58b7a2b22e253559247a815
Sepehr Parirokh <sepehrparirokh@gmail.com>
2019-10-27 00:04:16 -0700

    refactor and mulitlayers

--- public/js/main.js
+++ public/js/main.js
@@ -1,66 +1,38 @@
-// import SpriteSheet from './SpriteSheet.js';
+import Compositor from './Compositor.js'
+import { createBackgroundLayer } from './Layers.js'
 import { loadLevel } from './loaders.js';
 import { loadMarioSprite, loadBackgroundSprites } from './Sprites.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-class Compositor {
-  constructor() {
-    this.layers = []
-  }
-
-  draw(context) {
-    this.layers.forEach(layer => {
-      layer(context) // layer is a function that draws on the context
-    })
-  }
-}
-
-function createBackgroundLayer(backgrounds, sprites) {
-  const buffer = document.createElement('canvas')
-  buffer.width = 256;
-  buffer.height = 240;
-  backgrounds.forEach(background => {
-    drawBackground(background, buffer.getContext('2d'), sprites);
-  })
-  return function drawBackgroundLayer(context) {
-    context.drawImage(buffer, 0, 0)
-
-  }
-}
 
 function createSpriteLayer(sprite, pos) {
   return function drawSpriteLayer(context) {
-    sprite.draw('idle', context, pos.x, pos.y)
+    for (let i = 0; i < 20; ++i) {
+      sprite.draw('idle', context, pos.x + i * 16, pos.y)
+
+    }
   }
 }
 
-function drawBackground(background, context, sprites) {
-  background.ranges.forEach(([x1, x2, y1, y2]) => {
-    for (let x = x1; x < x2; ++x) {
-      for (let y = y1; y < y2; ++y) {
-        sprites.drawTile(background.tile, context, x, y);
-      }
-    }
-  });
-}
+
 
 
 Promise.all([
   loadMarioSprite(),
   loadBackgroundSprites(),
   loadLevel('1-1')
-]).then(([MarioSprite, sprites, level]) => {
+]).then(([MarioSprite, backgroundSprites, level]) => {
   const comp = new Compositor
-  const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites)
+  const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites)
   comp.layers.push(backgroundLayer)
 
 
 
   const pos = {
-    x: 64,
-    y: 64
+    x: 0,
+    y: 0
   }
 
   const spriteLayer = createSpriteLayer(MarioSprite, pos)
